50dbf0e0c7deae739ab4de1bd4d6fc777aecfe42
mirzaciri <mirza.ciri@gmail.com>
2023-04-05 16:56:33 +0200

    Update properties-extract.sh

--- .github/workflows/properties-extract.sh
+++ .github/workflows/properties-extract.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Check if YAML file and sandbox parameter are provided as arguments
+# Check if YAML file and env parameter are provided as arguments
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <yaml_file> <sandbox>"
+  echo "Usage: $0 <yaml_file> <env>"
   exit 1
 fi
 
-# Extract key-value pairs from specified sandbox section in YAML file using yq tool (you may need to install yq first)
+# Extract key-value pairs from specified env section in YAML file using yq tool (you may need to install yq first), github action supports this
 sandbox=$2
 values=$(yq eval ".$sandbox | to_entries | map(\"-D\" + .key + \"=\" + .value) | join(\" \")" "$1")
 
